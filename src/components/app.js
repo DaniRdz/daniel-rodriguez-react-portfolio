dc1a66d7d92bd15b198e9d1f2643efe91397af78
RdzCore <daniel_rod_con@hotmail.com>
2020-07-10 13:05:04 -0500

    add link and Route

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Blog from './pages/blog';
+import Contact from './pages/contact';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <BrowserRouter>
+          <div>
+            <NavigationContainer />
+            <Switch>
+              <Route exact path='/' component = {Home}/>
+              <Route exact path='/about-me' component = {About}/>
+              <Route exact path='/contact' component = {Contact}/>
+              <Route exact path='/blog' component = {Blog}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
         <h1>Daniel Rodríguez Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PortfolioContainer />
