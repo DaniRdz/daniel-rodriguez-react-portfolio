bca488cec8108773694c65ae4a75d5c732c19d2c
RdzCore <daniel_rod_con@hotmail.com>
2020-07-10 11:07:09 -0500

    add a filter fuction

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 
 import PortfolioItem from './portfolio-item';
-import portfolioItem from './portfolio-item';
 
 
 export default class PortfolioContainer extends Component {
-    constructor(){
+    constructor() {
         super();
-        console.log('Portfolio container is reandered');
+        this.state = {
+            pageTitle: 'Welcome to my portfolio',
+            data: [
+                { title: 'Some Enterprise', category: 'eCommerce' },
+                { title: 'Other Enterprise', category: 'Enterprise' },
+                { title: 'Some Project', category: 'Security' }
+            ]
+        };
+        this.handleFilter = this.handleFilter.bind(this);
     }
-    portfolioItems(){
-        const data = ['some Item', 'Other Item', 'someelse'];
 
-        return data.map(item =>{
-        return <PortfolioItem title = {item} url = {'google.com'} />
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
+    }
+
+    portfolioItems() {
+
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={'google.com'} />
         })
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <h2>Portfolio items goes here...</h2>
+                <h2>{this.state.pageTitle}</h2>
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+                <button onClick={() => this.handleFilter('Security')}>Security</button>
                 {this.portfolioItems()}
             </div>
         );
