1e5deee38d8baaeab9724b34b2f6fe7ddee32372
RdzCore <daniel_rod_con@hotmail.com>
2020-07-27 14:13:46 -0500

    add form component

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,37 +2,52 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
 
 export default class PortfolioManager extends Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
             portfolioItems: []
         }
+        this.handleSuccesfullFormSubmission = this.handleSuccesfullFormSubmission.bind(this);
+        this.handleFormSbmissionError = this.handleFormSbmissionError.bind(this);
     }
-    getPortfolioItems(){
-        axios.get('https://rdzcore.devcamp.space/portfolio/portfolio_items',{withCredentials: true})
-        .then(response =>{
-            this.setState({
-                portfolioItems: [...response.data.portfolio_items]
+    handleSuccesfullFormSubmission(portfolioItem) {
+        //TODO
+        //Update portfolioItems state
+        //and add portfolioItem to the list
+
+    }
+    handleFormSbmissionError(error) {
+        console.log('handleFormSbmissionError error', error);
+    }
+    getPortfolioItems() {
+        axios.get('https://rdzcore.devcamp.space/portfolio/portfolio_items', { withCredentials: true })
+            .then(response => {
+                this.setState({
+                    portfolioItems: [...response.data.portfolio_items]
+                })
             })
-        })
-        .catch(error => {
-            console.log('error in getPortfolioItems', error);
-        });
+            .catch(error => {
+                console.log('error in getPortfolioItems', error);
+            });
     }
-    componentDidMount(){
+    componentDidMount() {
         this.getPortfolioItems();
     }
     render() {
         return (
             <div className='portfolio-manager-wrapper'>
                 <div className='left-column'>
-                    <h1>Portfolio form...</h1>
+                    <PortfolioForm
+                        handleSuccesfullFormSubmission={this.handleSuccesfullFormSubmission}
+                        handleFormSbmissionError={this.handleFormSbmissionError}
+                    />
                 </div>
                 <div className='right-column'>
-                    <PortfolioSidebarList  data = {this.state.portfolioItems}/>
+                    <PortfolioSidebarList data={this.state.portfolioItems} />
                 </div>
             </div>
         );
