6bbc77d901d584a37e23c5279e62053529096248
RdzCore <daniel_rod_con@hotmail.com>
2020-07-27 10:56:39 -0500

    state with api call

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function () {
-    return (
-        <div>
-            <h1>Portfolio Manager</h1>
-        </div>
-    );
+export default class PortfolioManager extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            data: []
+        }
+    }
+    getPortfolioItems(){
+        axios.get('https://rdzcore.devcamp.space/portfolio/portfolio_items',{withCredentials: true})
+        .then(response =>{
+            this.setState({
+                data: [...response.data.portfolio_items]
+            })
+        })
+        .catch(error => {
+            console.log('error in getPortfolioItems', error);
+        });
+    }
+    componentDidMount(){
+        this.getPortfolioItems();
+    }
+    render() {
+        return (
+            <div className='portfolio-manager-wrapper'>
+                <div className='left-column'>
+                    <h1>Portfolio form...</h1>
+                </div>
+                <div className='right-column'>
+                    <h1>Portfolio items...</h1>
+                </div>
+            </div>
+        );
+    }
 }
