14e687692da2f5e218eed3898ceb48b135f3534f
RdzCore <daniel_rod_con@hotmail.com>
2020-08-04 11:24:29 -0500

    calling blog API

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class Blog extends Component {
     constructor() {
         super();
+
+        this.state = {
+            blogsItems : []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
     }
+
+    getBlogItems(){
+        axios.get('https://rdzcore.devcamp.space/portfolio/portfolio_blogs', { withCredentials: true})
+        .then(response =>{
+            this.setState({
+                blogsItems : response.data.portfolio_blogs
+            })
+        })
+        .catch(error =>{
+            console.log('getBlogItems error', error);
+        });
+    }
+
+    componentDidMount(){
+        this.getBlogItems();
+    }
+
     render() {
         return (
             <div>
