0b91d6f61203a81820a191beee9dc2cd504f4e6f
RdzCore <daniel_rod_con@hotmail.com>
2020-08-07 11:06:09 -0500

    creating blogs post via the  API

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BlogForm extends Component {
     constructor(props) {
@@ -13,8 +14,23 @@ export default class BlogForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    buildForm() {
+        let formData = new FormData();
+
+        formData.append('portfolio_blog[title]', this.state.title);
+        formData.append('portfolio_blog[blog_status]', this.state.blog_status);
+
+        return formData;
+    }
+
     handleSubmit(event) {
-        this.props.handleSuccessfulSubmit(this.state);
+        axios.post('https://rdzcore.devcamp.space/portfolio/portfolio_blogs', this.buildForm(), { withCredentials: true })
+            .then(response => {
+                this.props.handleSuccessfulSubmit(response.data);
+            })
+            .catch(error => {
+                console.log(' handleSubmit blog error', error);
+            });
         event.preventDefault();
     }
 
