d6fb14e82cef917ab98e255243873d6b030e248a
RdzCore <daniel_rod_con@hotmail.com>
2020-08-14 13:47:06 -0500

    Building About Page Styles

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
+import profilePicture from '../../../static/assets/images/bio/profile.jpg';
 
 export default function () {
     return (
         <div className='content-page-wrapper'>
-            <div className='left-column'>
-                <h1>Image goes here</h1>
-            </div>
-            <div className='rigth-column'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida urna dignissim ex iaculis, eu varius est mollis. Nulla aliquam leo felis, non venenatis sem commodo vitae. Suspendisse condimentum enim sit amet massa mollis, maximus sagittis odio malesuada. Sed varius lacinia purus, eu porta lorem. Aenean euismod pretium turpis, congue consectetur nulla efficitur in. Vestibulum dapibus scelerisque molestie. Morbi maximus dignissim ligula, eu sagittis sem molestie eu.
+            <div className='left-column'
+                style={{
+                    background: 'url(' + profilePicture + ') no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                }}
+            />
 
-                Aliquam vel hendrerit nisi, eget commodo urna. Sed sed elit nec massa mattis feugiat. Curabitur id mauris blandit, consequat lectus vel, pretium eros. Phasellus molestie ultrices magna, vitae viverra erat. Integer sed luctus enim. Proin malesuada vitae eros at cursus. Nunc lacinia rhoncus egestas. Nam a nulla elementum, posuere turpis et, ullamcorper velit. Nam eget lorem fringilla, cursus lectus a, feugiat dui. Aliquam erat volutpat. Cras a mollis velit.
-
-                Curabitur commodo lacinia tellus. Nunc condimentum mollis neque, non placerat leo feugiat ut. Maecenas sed elit justo. Aenean pretium vitae nisl sit amet aliquet. Praesent ornare mi at odio efficitur iaculis. Quisque gravida malesuada nulla, ut aliquam nisl. Maecenas semper interdum est sed hendrerit. Morbi sed malesuada lorem, sollicitudin interdum massa. Maecenas et lacinia libero. Fusce convallis rhoncus tincidunt. Pellentesque tincidunt lacinia elit eget consequat.
+            <div className='right-column'>
+                Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
+                dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
+                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget
+                risus varius blandit sit amet non magna. Morbi leo risus, porta ac
+                consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida
+                at eget metus. Donec sed odio dui. Cras mattis consectetur purus sit
+                amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nulla
+                vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque
+                ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non
+                commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
+                elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur
+                et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+                commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
+                risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
+                consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis
+                euismod semper.
             </div>
         </div>
     );
