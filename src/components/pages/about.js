ca212ece2700c7ad47b7360b1e8948e504f2be60
RdzCore <daniel_rod_con@hotmail.com>
2020-08-14 13:03:37 -0500

    Enabling Authorization Rules for Blog Detail Component

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 
-export default function(){
-    return(
-        <div>
-            About...
+export default function () {
+    return (
+        <div className='content-page-wrapper'>
+            <div className='left-column'>
+                <h1>Image goes here</h1>
+            </div>
+            <div className='rigth-column'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida urna dignissim ex iaculis, eu varius est mollis. Nulla aliquam leo felis, non venenatis sem commodo vitae. Suspendisse condimentum enim sit amet massa mollis, maximus sagittis odio malesuada. Sed varius lacinia purus, eu porta lorem. Aenean euismod pretium turpis, congue consectetur nulla efficitur in. Vestibulum dapibus scelerisque molestie. Morbi maximus dignissim ligula, eu sagittis sem molestie eu.
+
+                Aliquam vel hendrerit nisi, eget commodo urna. Sed sed elit nec massa mattis feugiat. Curabitur id mauris blandit, consequat lectus vel, pretium eros. Phasellus molestie ultrices magna, vitae viverra erat. Integer sed luctus enim. Proin malesuada vitae eros at cursus. Nunc lacinia rhoncus egestas. Nam a nulla elementum, posuere turpis et, ullamcorper velit. Nam eget lorem fringilla, cursus lectus a, feugiat dui. Aliquam erat volutpat. Cras a mollis velit.
+
+                Curabitur commodo lacinia tellus. Nunc condimentum mollis neque, non placerat leo feugiat ut. Maecenas sed elit justo. Aenean pretium vitae nisl sit amet aliquet. Praesent ornare mi at odio efficitur iaculis. Quisque gravida malesuada nulla, ut aliquam nisl. Maecenas semper interdum est sed hendrerit. Morbi sed malesuada lorem, sollicitudin interdum massa. Maecenas et lacinia libero. Fusce convallis rhoncus tincidunt. Pellentesque tincidunt lacinia elit eget consequat.
+            </div>
         </div>
     );
 }
