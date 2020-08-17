e0a21db9a6179592aac111436bf95fccb72c4e14
RdzCore <daniel_rod_con@hotmail.com>
2020-08-17 11:30:38 -0500

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default function (props) {
-    return (
-        <div>
-            <h2>Portfolio Detail For {props.match.params.slug}</h2>
-        </div>
-    );
+export default class PortfolioDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.getItem = this.getItem.bind(this);
+    }
+
+    componentDidMount(){
+        this.getItem();
+    }
+
+    getItem(){
+        axios.get(`https://rdzcore.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, {withCredentials: true})
+        .then(response =>{
+            console.log('response', response);
+        })
+        .catch(error =>{
+            console.log('getItem error', error);
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Portfolio Detail For {this.props.match.params.slug}</h2>
+            </div>
+        );
+    }
 }
