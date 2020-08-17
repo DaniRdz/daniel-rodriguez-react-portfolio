c1603787ac836de4c451619132fa286eec4b26ef
RdzCore <daniel_rod_con@hotmail.com>
2020-08-17 11:55:24 -0500

    Rendering the Portfolio Detail Data on the Screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -5,27 +5,43 @@ export default class PortfolioDetail extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            portfolioItem: {}
+        }
+
         this.getItem = this.getItem.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getItem();
     }
 
-    getItem(){
-        axios.get(`https://rdzcore.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, {withCredentials: true})
-        .then(response =>{
-            console.log('response', response);
-        })
-        .catch(error =>{
-            console.log('getItem error', error);
-        })
+    getItem() {
+        axios.get(`https://rdzcore.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true })
+            .then(response => {
+                this.setState({
+                    portfolioItem: response.data.portfolio_item
+                })
+            })
+            .catch(error => {
+                console.log('getItem error', error);
+            })
     }
-    
+
     render() {
+        const {
+            banner_image_url,
+            category,
+            description,
+            logo_url,
+            name,
+            thumb_image_url,
+            url
+        } = this.state.portfolioItem;
         return (
             <div>
-                <h2>Portfolio Detail For {this.props.match.params.slug}</h2>
+                <h2>{name}</h2>
+                <p>{description}</p>
             </div>
         );
     }
