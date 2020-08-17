02c63cb2afec9aa77ba4d548e5c87e65bc905d43
RdzCore <daniel_rod_con@hotmail.com>
2020-08-17 13:20:45 -0500

    Portfolio Detail Styles

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -38,10 +38,34 @@ export default class PortfolioDetail extends Component {
             thumb_image_url,
             url
         } = this.state.portfolioItem;
+
+        const bannerStyles = {
+            background: 'url('+ banner_image_url +') no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }
+
+        const logoStyle = {
+            width: '200px'
+        }
         return (
-            <div>
-                <h2>{name}</h2>
-                <p>{description}</p>
+            <div className='portfolio-detail-wrapper'>
+                <div className='banner' style={bannerStyles}>
+                    <img src={logo_url} style={logoStyle}/>
+                </div>
+
+                <div className='portfolio-detail-description-wrapper'>
+                    <div className='description'>
+                        {description}
+                    </div>
+                </div>
+
+                <div className='bottom-content-wrapper'>
+                    <a href={url} className='site-link' target='_blank'>
+                        Visit {name}
+                    </a>
+                </div>
+
             </div>
         );
     }
